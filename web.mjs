d9a038bf19038d898ec13321e159dd40a823b9e6
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-03-17 07:45:33 +0000

    Deploying to gh-pages from @ b-on-g/pazzle@0a16295dc4b2223f40ebd432e70a45d0bb89cb5b 🚀

--- web.mjs
+++ web.mjs
@@ -9433,6 +9433,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
